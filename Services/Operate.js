0169f32a1e183897c1b761cad4b5ae79c1c35bef
damilare1 <damilare182@gmail.com>
2021-03-27 12:01:21 +0100

    grouped files and used complicated html

---
+++ Services/Operate.js
@@ -0,0 +1,50 @@
+class Operate {
+    static isArray(value) {
+      return (
+        value &&
+        Array.isArray(value) &&
+        typeof value === "object" &&
+        value.constructor === Array
+      );
+    }
+  
+    static isIterable(obj) {
+      // checks for null and undefined
+      if (obj == null) {
+        return false;
+      }
+      return typeof obj[Symbol.iterator] === 'function';
+    }
+  
+    static isObject(value) { return value && typeof value === 'object' && value.constructor === Object; }
+  
+    static isIn(argA, argB) {
+      return argB.indexOf(argA) > -1 ? true : false;
+    }
+  
+    static isString(str) {
+      return typeof str === 'string';
+    }
+  
+    static isPromise(obj) {
+      return typeof obj === "object" && "then" in obj;
+    }
+    static isFlowRequest(obj) {
+      return "flowRequest" in obj;
+    }
+    static handlePromise(process) {
+      if (this.isPromise(process)) {
+        process
+          .then(function (res) {
+            return res;
+          })
+          .catch(function (err) {
+            return err;
+          });
+      } else {
+        return new Promise(function (resolve, _) {
+          resolve(process);
+        });
+      }
+    }
+  }
